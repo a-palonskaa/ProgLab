2fe5d6b8bebc266d34ca99339921a51b9614a8c3
vanomark <rudiger.kaspar@mail.ru>
2024-09-21 21:09:51 +0300

    Files for result function

--- result.c
+++ result.c
@@ -1,7 +1,7 @@
 #include "result.h"
 
-float result(int K, float resistance[])
-{
+float result(int K, float resistance[]) 
+{   
     assert(resistance);
     assert(isfinite(K));
 
@@ -9,14 +9,14 @@ float result(int K, float resistance[])
     float deviation        = 0;
 
     int i = 0;
-    for (i = 0; i < K; i++)
+    for (i = 0; i < K; i++) 
         resistance_final += resistance[i];
     resistance_final /= K;
-
+    
     int j = 0;
-    for (j = 0; j < K; j++)
-        deviation += (resistance[j] - resistance_final) * (resistance[j] - resistance_final);
-    deviation = sqrt(deviation) / K;
+    for (j = 0; j < K; j++) 
+        deviation += (resistance[j] - resistance_final)*(resistance[j] - resistance_final);
+    deviation = sqrt(deviation)/K;   
 
     printf("%.4f +- %.4f\n", resistance_final, deviation);
 
--- result.h
+++ result.h
@@ -1,5 +1,4 @@
 #include <math.h>
 #include <stdio.h>
 #include <assert.h>
-
 float result(int K, float *resistance);
